7d7722646bb2b0254ea69e314fbb0419875edf09
IgooorGP <igor.feq@gmail.com>
2020-12-31 00:09:32 -0300

    improve sonar properties file and add junit xml

--- scripts/start-tests.sh
+++ scripts/start-tests.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 SRC_PROJECT_PATH=algorithms
-TESTS_PROJECT_PATH=tests/
+TESTS_PROJECT_PATH=tests
 
 echo "Running pytest and generating xml coverage report..."
-pytest --cov=$SRC_PROJECT_PATH --cov-report xml --cov-report term $TESTS_PROJECT_PATH
+pytest $TESTS_PROJECT_PATH \
+    --cov=$SRC_PROJECT_PATH \
+    --cov-report xml \
+    --cov-report term \
+    --junitxml=test-results.xml
