047db3b8d186e3c596cea1560828842baf041bcb
IgooorGP <igor.feq@gmail.com>
2021-04-30 20:48:13 -0300

    improve undirect graph api and its tests

--- algorithms/graphs/undirected/adjacency_sets.py
+++ algorithms/graphs/undirected/adjacency_sets.py
@@ -6,7 +6,6 @@ from __future__ import annotations
 
 from typing import Dict
 from typing import Generic
-from typing import Optional
 from typing import Set
 from typing import TypeVar
 
@@ -38,55 +37,77 @@ class Graph(Generic[T]):
     def edges(self) -> int:
         return self._edges_count
 
-    def adjacent(self, vertex: T) -> Set[T]:
+    def has_vertex(self, vertex: T) -> bool:
+        """
+        Returns True if a given vertex is in the graph (vertex is in the inner hashmap of vertices).
+        Otherwise, returns False.
+        """
+        return vertex in self._vertices
+
+    def get_vertex(self, vertex: T) -> T:
+        """
+        Returns a vertex from the graph if it exists. If it does not, then VertexNotFoundError is raised.
+        """
+        if self.has_vertex(vertex):
+            return vertex
+
+        raise VertexNotFoundError()
+
+    def get_adjacent_vertices(self, vertex: T) -> Set[T]:
         """
         Retrives a set of the adjacent vertices of a given vertex. Raises VertexNotFoundError
         if the vertex cannot be found in the graph.
         """
-        adj_set = self._get_adjacent_set(vertex)
-
-        if adj_set is None:
+        if not self.has_vertex(vertex):
             raise VertexNotFoundError(vertex)
 
-        return adj_set
+        return self._vertices[vertex]
+
+    def add_vertex(self, new_vertex: T) -> None:
+        """
+        Adds a new vertex to the graph if it does not already exists. If it already exists, then
+        nothing is done.
+        """
+        if not self.has_vertex(new_vertex):
+            self._vertices_count += 1
+            self._create_adjacent_set(new_vertex)
+
+    def has_edge(self, vertex_1: T, vertex_2: T) -> bool:
+        """
+        Returns True if there's an edge between the two vertices. If the vertices do not exist, naturally
+        this method returns False as there can be no edge without one of the vertices.
+        """
+        if not self.has_vertex(vertex_1) or not self.has_vertex(vertex_2):
+            return False
+
+        return vertex_1 in self.get_adjacent_vertices(vertex_2)
 
     def add_edge(self, vertex_1: T, vertex_2: T) -> None:
         """
         Adds a new connection (edge) between two vertices in the graph. If the vertices do not exist,
         then they are created and then connected. If they exist, just the edge connection is made.
         """
-        adj_set_1 = self._get_adjacent_set(vertex_1)
-        adj_set_2 = self._get_adjacent_set(vertex_2)
+        if self.has_edge(vertex_1, vertex_2):
+            return  # edge already exists, return (no edge anomalies are allowed with adjacency sets)
 
-        # create new adj set if the vertex is not in the graph already
-        if adj_set_1 is None:
-            self._vertices_count += 1  # new vertex in the graph
-            adj_set_1 = self._create_adjacent_set(vertex_1)
+        if not self.has_vertex(vertex_1):
+            self.add_vertex(vertex_1)
 
-        if adj_set_2 is None:
-            self._vertices_count += 1
-            adj_set_2 = self._create_adjacent_set(vertex_2)
+        if not self.has_vertex(vertex_2):
+            self.add_vertex(vertex_2)
 
-        # if one vertex does not reference the other -> new edge will be added!
-        if vertex_1 not in adj_set_2:
-            self._edges_count += 1
+        # vertices and adjacent vertices are expected to exist from here on
+        adj_set_1 = self.get_adjacent_vertices(vertex_1)
+        adj_set_2 = self.get_adjacent_vertices(vertex_2)
 
+        # adds the new edge between the two vertices
+        self._edges_count += 1
         adj_set_1.add(vertex_2)
         adj_set_2.add(vertex_1)
 
-    def _get_adjacent_set(self, vertex: T) -> Optional[Set[T]]:
-        """
-        Retrives the adjacency set of a given vertex from the vertex-to-adjacent-vertices mapping. If the vertex
-        does not exist, it returns None.
-        """
-        if vertex in self._vertices:
-            return self._vertices[vertex]
-
-        return None
-
     def _create_adjacent_set(self, vertex: T) -> Set[T]:
         """
-        Adds a new vertex and an empty adjacency set to the vertex-to-adjacent-vertices mapping.
+        Adds a new vertex and an empty adjacency set to the vertex-to-adjacent-vertices hashmap.
         """
         new_adj_set: Set[T] = set()
         self._vertices[vertex] = new_adj_set
--- tests/graphs/undirected/test_adjacency_sets.py
+++ tests/graphs/undirected/test_adjacency_sets.py
@@ -34,6 +34,54 @@ def triangular_graph_fixture() -> Graph[str]:
     return graph
 
 
+@pytest.fixture
+def key_graph_fixture() -> Graph[str]:
+    r"""
+    Builds a graph fixture with six vertices and seven edges in a format similar
+    to a key.
+
+      1
+     / \
+    2 - 3
+     \ /
+      4
+      | \
+      5 - 6
+
+    [1] -> [2, 3]
+    [2] -> [1, 3, 4]
+    [3] -> [1, 2, 4]
+    [4] -> [2, 3, 5, 6]
+    [5] -> [4, 6]
+    [6] -> [4, 5]
+    """
+    graph: Graph[str] = Graph()  # empty graph
+
+    graph.add_edge("1", "2")
+    graph.add_edge("1", "3")
+
+    graph.add_edge("2", "1")
+    graph.add_edge("2", "3")
+    graph.add_edge("2", "4")
+
+    graph.add_edge("3", "1")
+    graph.add_edge("3", "2")
+    graph.add_edge("3", "4")
+
+    graph.add_edge("4", "2")
+    graph.add_edge("4", "3")
+    graph.add_edge("4", "5")
+    graph.add_edge("4", "6")
+
+    graph.add_edge("5", "4")
+    graph.add_edge("5", "6")
+
+    graph.add_edge("6", "4")
+    graph.add_edge("6", "5")
+
+    return graph
+
+
 def test_should_create_new_graph_5_vertices_6_edges():
     r"""
     Expected graph:
@@ -80,11 +128,39 @@ def test_should_create_new_graph_5_vertices_6_edges():
     assert graph.vertices == 5
     assert graph.edges == 6
 
-    assert graph.adjacent("1") == {"2", "3"}
-    assert graph.adjacent("2") == {"1", "3", "4"}
-    assert graph.adjacent("3") == {"1", "2", "4", "5"}
-    assert graph.adjacent("4") == {"2", "3"}
-    assert graph.adjacent("5") == {"3"}
+    assert graph.get_adjacent_vertices("1") == {"2", "3"}
+    assert graph.get_adjacent_vertices("2") == {"1", "3", "4"}
+    assert graph.get_adjacent_vertices("3") == {"1", "2", "4", "5"}
+    assert graph.get_adjacent_vertices("4") == {"2", "3"}
+    assert graph.get_adjacent_vertices("5") == {"3"}
+
+    assert graph.has_vertex("1")
+    assert graph.has_vertex("2")
+    assert graph.has_vertex("3")
+    assert graph.has_vertex("4")
+    assert graph.has_vertex("5")
+    assert graph.has_vertex("6") is False
+
+    assert graph.has_edge("1", "2")
+    assert graph.has_edge("1", "3")
+
+    assert graph.has_edge("2", "1")
+    assert graph.has_edge("2", "3")
+    assert graph.has_edge("2", "4")
+
+    assert graph.has_edge("3", "1")
+    assert graph.has_edge("3", "2")
+    assert graph.has_edge("3", "4")
+    assert graph.has_edge("3", "5")
+
+    assert graph.has_edge("4", "2")
+    assert graph.has_edge("4", "3")
+
+    assert graph.has_edge("5", "3")
+
+    assert graph.has_edge("5", "1") is False
+    assert graph.has_edge("5", "4") is False
+    assert graph.has_edge("2", "5") is False
 
 
 def test_should_create_new_graph_5_vertices_8_edges():
@@ -137,11 +213,16 @@ def test_should_create_new_graph_5_vertices_8_edges():
     assert graph.vertices == 5
     assert graph.edges == 8
 
-    assert graph.adjacent("1") == {"2", "3", "5"}
-    assert graph.adjacent("2") == {"1", "3", "4"}
-    assert graph.adjacent("3") == {"1", "2", "4", "5"}
-    assert graph.adjacent("4") == {"2", "3", "5"}
-    assert graph.adjacent("5") == {"1", "3", "4"}
+    assert graph.get_adjacent_vertices("1") == {"2", "3", "5"}
+    assert graph.get_adjacent_vertices("2") == {"1", "3", "4"}
+    assert graph.get_adjacent_vertices("3") == {"1", "2", "4", "5"}
+    assert graph.get_adjacent_vertices("4") == {"2", "3", "5"}
+    assert graph.get_adjacent_vertices("5") == {"1", "3", "4"}
+
+    assert graph.has_edge("5", "1")
+    assert graph.has_edge("5", "3")
+    assert graph.has_edge("5", "4")
+    assert graph.has_edge("5", "2") is False
 
 
 def test_should_raise_exception_when_vertex_not_found_for_adjacency(triangular_graph_fixture: Graph[str]):
@@ -149,9 +230,9 @@ def test_should_raise_exception_when_vertex_not_found_for_adjacency(triangular_g
     graph = triangular_graph_fixture
 
     # act and assert
-    assert graph.adjacent("1") == {"2", "3"}
-    assert graph.adjacent("2") == {"1", "3"}
-    assert graph.adjacent("3") == {"1", "2"}
+    assert graph.get_adjacent_vertices("1") == {"2", "3"}
+    assert graph.get_adjacent_vertices("2") == {"1", "3"}
+    assert graph.get_adjacent_vertices("3") == {"1", "2"}
 
     with pytest.raises(VertexNotFoundError):
-        graph.adjacent("5")
+        graph.get_adjacent_vertices("5")
