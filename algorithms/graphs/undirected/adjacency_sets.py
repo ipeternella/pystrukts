561c73164d2a2d5663cdc973a926eae3ca8173c9
IgooorGP <igor.feq@gmail.com>
2021-04-29 23:02:23 -0300

    add better docstrings to adj set graph

--- algorithms/graphs/undirected/adjacency_sets.py
+++ algorithms/graphs/undirected/adjacency_sets.py
@@ -17,30 +17,18 @@ T = TypeVar("T")
 
 class Graph(Generic[T]):
     """
-    A Graph implementation using adjacent sets and hashmaps (symbol tables). An adjacency list is used
-    to prevent repeated edges between vertices (parallel edges).
+    A Graph implementation using adjacent sets and hashmaps (symbol tables). An adjacency set is used
+    to prevent repeated edges between vertices (parallel edges -- anomally).
     """
 
     _vertices_count: int  # total amount of vertices (nodes)
     _edges_count: int  # total amount of edges
-    _vertices: Dict[T, Set[T]]  # adjacency set
+    _vertices: Dict[T, Set[T]]  # vertex-to-adjacent-vertices mapping
 
     def __init__(self) -> None:
         self._vertices_count = 0
         self._edges_count = 0
         self._vertices = {}  # symbol table / hashmap
-
-    def _get_adjacent_set(self, vertex: T) -> Optional[Set[T]]:
-        if vertex in self._vertices:
-            return self._vertices[vertex]
-
-        return None
-
-    def _create_adjacent_set(self, vertex: T) -> Set[T]:
-        new_adj_set: Set[T] = set()
-        self._vertices[vertex] = new_adj_set
-
-        return new_adj_set
 
     @property
     def vertices(self) -> int:
@@ -51,6 +39,10 @@ class Graph(Generic[T]):
         return self._edges_count
 
     def adjacent(self, vertex: T) -> Set[T]:
+        """
+        Retrives a set of the adjacent vertices of a given vertex. Raises VertexNotFoundError
+        if the vertex cannot be found in the graph.
+        """
         adj_set = self._get_adjacent_set(vertex)
 
         if adj_set is None:
@@ -59,6 +51,10 @@ class Graph(Generic[T]):
         return adj_set
 
     def add_edge(self, vertex_1: T, vertex_2: T) -> None:
+        """
+        Adds a new connection (edge) between two vertices in the graph. If the vertices do not exist,
+        then they are created and then connected. If they exist, just the edge connection is made.
+        """
         adj_set_1 = self._get_adjacent_set(vertex_1)
         adj_set_2 = self._get_adjacent_set(vertex_2)
 
@@ -78,5 +74,21 @@ class Graph(Generic[T]):
         adj_set_1.add(vertex_2)
         adj_set_2.add(vertex_1)
 
-    def __len__(self) -> int:
-        return self._vertices_count
+    def _get_adjacent_set(self, vertex: T) -> Optional[Set[T]]:
+        """
+        Retrives the adjacency set of a given vertex from the vertex-to-adjacent-vertices mapping. If the vertex
+        does not exist, it returns None.
+        """
+        if vertex in self._vertices:
+            return self._vertices[vertex]
+
+        return None
+
+    def _create_adjacent_set(self, vertex: T) -> Set[T]:
+        """
+        Adds a new vertex and an empty adjacency set to the vertex-to-adjacent-vertices mapping.
+        """
+        new_adj_set: Set[T] = set()
+        self._vertices[vertex] = new_adj_set
+
+        return new_adj_set
