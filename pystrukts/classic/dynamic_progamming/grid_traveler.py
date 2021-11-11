2b2c517174aadae0f410a014f338dd14b6c918eb
IgooorGP <igor.feq@gmail.com>
2021-11-11 13:49:24 -0300

    refactor(classic): improve dp for grid traveler

--- pystrukts/classic/dynamic_progamming/grid_traveler.py
+++ pystrukts/classic/dynamic_progamming/grid_traveler.py
@@ -1,23 +1,21 @@
 """
 Module with grid-traveling problems which can be solved with dynamic programming.
+
+Problem:
+    Given a 2D (m x n) grid that one wants to travel from top-left corner to the
+    bottom-right corner and the restriction that it's only possible to move down
+    or right, compute the amount of possible ways to reach the bottom-right destination.
 """
 
 from typing import Dict
-from typing import List
-from typing import Optional
 from typing import Tuple
 
 Pair = Tuple[int, int]
 
 
-def count_possible_ways_grid2D_bruteforce(m: int, n: int) -> int:
+def count_ways_grid2d_bruteforce(m: int, n: int) -> int:
     """
-    Given a 2D (m x n) grid that one wants to travel from top-left corner to the
-    bottom-right corner and the restriction that it's only possible to move down
-    or right, this procedure computes the amount of possible ways to reach the
-    bottom-right destination.
-
-    This algorithm is the naive/brute-force solution and will get stuck for larger
+    Naive/brute-force solution for the 2D grid traveler problem. It will "get stuck" for larger
     inputs as the time complexity is O(2 ^ (n + m)).
     """
     if m == 0 or n == 0:
@@ -26,41 +24,41 @@ def count_possible_ways_grid2D_bruteforce(m: int, n: int) -> int:
     if m == 1 or n == 1:
         return 1
 
-    return count_possible_ways_grid2D_bruteforce(m, n - 1) + count_possible_ways_grid2D_bruteforce(m - 1, n)
+    return count_ways_grid2d_bruteforce(m, n - 1) + count_ways_grid2d_bruteforce(m - 1, n)
 
 
-def count_possible_ways_grid2D_memoized(m: int, n: int, memory: Optional[Dict[Pair, int]] = None) -> int:
+def count_ways_grid2d_memoized(m: int, n: int) -> int:
     """
     Optimization of the 2D grid traveler problem using memoization.
     """
-    pair = (m, n)
 
-    # memoization
-    if memory is None:
-        memory = dict()
+    def count_ways_grid2d_helper(m: int, n: int, memory: Dict[Pair, int]) -> int:
+        pair = (m, n)
 
-    if memo := memory.get(pair):
-        return memo
+        if pair in memory:
+            return memory[pair]
 
-    # trivial cases
-    if m == 0 or n == 0:
-        return 0
+        if m == 0 or n == 0:
+            return 0
 
-    if m == 1 or n == 1:
-        return 1
+        if m == 1 or n == 1:
+            return 1
 
-    # recursion
-    memory[pair] = count_possible_ways_grid2D_memoized(m, n - 1, memory) + count_possible_ways_grid2D_memoized(
-        m - 1, n, memory
-    )
+        memory[pair] = count_ways_grid2d_helper(m, n - 1, memory) + count_ways_grid2d_helper(m - 1, n, memory)
+        return memory[pair]
 
-    return memory[pair]
+    return count_ways_grid2d_helper(m, n, dict())
 
 
-def count_possible_ways_grid2D_bottomup(m: int, n: int) -> int:
-    # trivial solutions for bottomup
-    solutions: List[List[int]] = [[0] * n] * m
-    solutions[0][0] = 0
-    solutions[0][1] = 0
-    solutions[1][0] = 0
-    solutions[1][1] = 1
+def count_ways_grid2d_bottomup(m: int, n: int) -> int:
+    """
+    Optimization of the 2D grid traveler problem using tabulation.
+    """
+    solutions = [[0] * (n + 1) for _ in range(m + 1)]  # 2d tabulation
+    solutions[0][1] = 1  # this OR [1][0] must be 1 (never both)
+
+    for row in range(1, m + 1):
+        for col in range(1, n + 1):
+            solutions[row][col] = solutions[row - 1][col] + solutions[row][col - 1]
+
+    return solutions[m][n]
--- tests/classic/dynamic/test_grid_traveler.py
+++ tests/classic/dynamic/test_grid_traveler.py
@@ -3,8 +3,9 @@ Module with tests for grid traveler problems.
 """
 import unittest
 
-from pystrukts.classic.dynamic_progamming.grid_traveler import count_possible_ways_grid2D_bruteforce
-from pystrukts.classic.dynamic_progamming.grid_traveler import count_possible_ways_grid2D_memoized
+from pystrukts.classic.dynamic_progamming.grid_traveler import count_ways_grid2d_bottomup
+from pystrukts.classic.dynamic_progamming.grid_traveler import count_ways_grid2d_bruteforce
+from pystrukts.classic.dynamic_progamming.grid_traveler import count_ways_grid2d_memoized
 
 
 class DynamicGridTravelerTestSuite(unittest.TestCase):
@@ -12,7 +13,7 @@ class DynamicGridTravelerTestSuite(unittest.TestCase):
     Grid traveler problem test suite.
     """
 
-    def test_should_count_possible_ways_grid2D_bruteforce_on_2x3(self):
+    def test_should_count_ways_grid2d_bruteforce_on_2x3(self):
         """
         Should count the possible ways to travel on a 2D grid 2x3 with the brute-force algorithm.
         """
@@ -21,7 +22,7 @@ class DynamicGridTravelerTestSuite(unittest.TestCase):
         n = 3
 
         # act
-        amount_ways = count_possible_ways_grid2D_bruteforce(m, n)
+        amount_ways = count_ways_grid2d_bruteforce(m, n)
 
         # assert
         self.assertEqual(amount_ways, 3)
@@ -35,7 +36,35 @@ class DynamicGridTravelerTestSuite(unittest.TestCase):
         n = 18
 
         # act
-        amount_ways = count_possible_ways_grid2D_memoized(m, n)
+        amount_ways = count_ways_grid2d_memoized(m, n)
+
+        # assert
+        self.assertEqual(amount_ways, 2333606220)
+
+    def test_should_count_possible_ways_grid2D_memoized_on_18x18(self):
+        """
+        Should quickly compute possible ways for a large grid of 18x18 using memoized optimization.
+        """
+        # arrange
+        m = 18
+        n = 18
+
+        # act
+        amount_ways = count_ways_grid2d_memoized(m, n)
+
+        # assert
+        self.assertEqual(amount_ways, 2333606220)
+
+    def test_should_count_possible_ways_grid2D_bottomup_on_18x18(self):
+        """
+        Should quickly compute possible ways for a large grid of 18x18 using bottomup optimization.
+        """
+        # arrange
+        m = 18
+        n = 18
+
+        # act
+        amount_ways = count_ways_grid2d_bottomup(m, n)
 
         # assert
         self.assertEqual(amount_ways, 2333606220)
